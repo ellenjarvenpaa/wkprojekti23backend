35adf61446d06c7fe94eb7f0f4f1133479b7a314
liem nguyen <elminguyen@gmail.com>
2023-11-23 14:23:36 +0200

    deploy

--- ucad23-express-db/src/controllers/dish-controller.mjs
+++ ucad23-express-db/src/controllers/dish-controller.mjs
@@ -2,32 +2,32 @@ import { fetchAllDishes } from "../models/dish-model.mjs";
 
 const getDishes = async (req, res) => {
 	const rows = await fetchAllDishes();
-	const categories = [];
-		// create caategories array
-		rows.forEach((row) => {
-			if (!categories.includes(row.category_name)) {
-				categories.push(row.category_name);
-			}
-		});
-		console.log('categories includes', categories);
+	// const categories = [];
+	// 	// create caategories array
+	// 	rows.forEach((row) => {
+	// 		if (!categories.includes(row.category_name)) {
+	// 			categories.push(row.category_name);
+	// 		}
+	// 	});
+	// 	console.log('categories includes', categories);
 
-		// create object that includes category name and dishes of that caategory
-		categories.forEach((category) => {
+	// 	// create object that includes category name and dishes of that caategory
+	// 	categories.forEach((category) => {
 
-			const dishes = [];
-			rows.forEach((row) => {
+	// 		const dishes = [];
+	// 		rows.forEach((row) => {
 
-				if (row.category_name === category) {
-					delete row.category_name;
-					pullatDishes.push(row);
-				}
-			});
-			const resultPullat = {
-				category_name: 'pullat',
-				dishes: pullatDishes
-			};
-			console.log(resultPullat);
-		});
+	// 			if (row.category_name === category) {
+	// 				delete row.category_name;
+	// 				pullatDishes.push(row);
+	// 			}
+	// 		});
+	// 		const resultPullat = {
+	// 			category_name: 'pullat',
+	// 			dishes: pullatDishes
+	// 		};
+	// 		console.log(resultPullat);
+	// 	});
 
 	res.json(rows);
 };
